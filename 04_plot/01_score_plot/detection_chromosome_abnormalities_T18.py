b9db771a3097813c43da9f168b4ef902c4603f07
eastbrain <140467225+eastbrain@users.noreply.github.com>
2024-01-04 17:05:21 +0900

    Add files via upload

---
+++ 04_plot/01_score_plot/detection_chromosome_abnormalities_T18.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import argparse
+import warnings
+import scipy
+import scipy.stats as ss
+import pandas as pd
+import numpy as np 
+import matplotlib
+import matplotlib.pyplot as plt
+
+import matplotlib
+matplotlib.use('TkAgg')
+#matplotlib.use('Agg')
+
+#####################################################################################################
+def plot_anomaly(df, threshold, title='KF-score'):
+    #df = df.sort_index(ascending=True)
+    df = df.sort_index(ascending=False)
+    ranks = np.linspace(1, len(df), len(df))
+    outliers = (df > threshold)
+    t18 = df[df>3]
+
+    for i in t18.index:
+      score = round(t18[i],4)
+      id = i.replace("s_","")
+      print((id) + ": "+ str(score))
+
+    plt.figure(dpi=150)
+    plt.plot(ranks[outliers],  df[outliers],'o', color='r',label='trisomy 18')
+    plt.plot(ranks[~outliers], df[~outliers],'o', color='b', label='normal')
+    plt.axhline(threshold,color='r',label='threshold', alpha=0.5)
+    plt.legend(loc = 'upper right')
+    plt.title(title, fontweight='bold')
+    plt.xlabel('Edwards syndrome')
+    plt.ylabel('KF-score')
+    plt.xticks([])
+    plt.show()
+
+#####################################################################################################
+def draw_chart(mat_file):
+   df = pd.read_csv(mat_file, sep = "\t").set_index('chr').loc['chr18']
+   plot_anomaly(df,3) 
+
+
+#####################################################################################################
+def main():
+  warnings.filterwarnings('ignore')
+
+  parser = argparse.ArgumentParser(description='Welcoming version (sckacker@ajou.ac.kr, gtphrase@eonelab.co.kr)')
+  parser.add_argument('mat' ,type=str,help='matrix.txt or table.txt')
+
+  args = parser.parse_args(sys.argv[1:])
+  mat_file = args.mat
+  draw_chart(mat_file)
+
+if __name__ == '__main__':
+  main()
