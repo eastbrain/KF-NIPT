8fc9f794a87f098f6bf2af9bd5e57f7bd4a5755c
eastbrain <140467225+eastbrain@users.noreply.github.com>
2024-01-23 15:15:27 +0900

    Create cnvkit_zsocre_EONE.py

---
+++ 01_utils/cnvkit_zsocre_EONE.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import pysam
+import logging
+import warnings
+
+
+#####################################################################################################
+def calculate_zscore(args):
+  cnr_file  = args.cnr
+  sample_nm = os.path.basename(cnr_file).split(".")[0] 
+  cnr = pd.read_csv(cnr_file,sep="\t")
+  cnr = cnr[cnr['chromosome'] == 'chr21']
+
+  sd = np.sqrt(1 - cnr['weight'])
+  zscore =  cnr['log2'] / sd
+  print(sample_nm,zscore.max())
+#####################################################################################################
+def main():
+  warnings.filterwarnings('ignore')
+
+  parser = argparse.ArgumentParser(description='NIPT Welcoming version (gtphrase@eonelab.co.kr)')
+  parser.add_argument('cnr' ,type=str,help='cnvkit output cnr')
+
+  args = parser.parse_args(sys.argv[1:])
+  calculate_zscore(args)
+
+if __name__ == '__main__':
+    main()
