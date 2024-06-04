6e7ed201ab1f2ac17f507c8f30d2c20d867dabf1
eastbrain <140467225+eastbrain@users.noreply.github.com>
2024-06-04 20:48:14 +0900

    Delete 05_plot/02_violin_plot directory

--- 05_plot/02_violin_plot/demo_set/use.cmd
+++
@@ -1,4 +0,0 @@
-bedtools makewindows -g hg19.chrom.sizes  -w 100000 |  bedtools coverage -g hg19.chrom.sizes  -b SRR5975172.F4.q20.sorted.bam.markdup.rmdup.bam  -a -  -nonamecheck
-
-genome_nuc_comp = !bedtools makewindows -g reference/hg38.chrom.sizes -w 100000 | bedtools nuc -fi reference/hg38.fa -bed -
-
